70fb4bfe064ff0039b58dc2f101e9cdc6c9ca9de
TheLegendarySpark <thelegendspk101@gmail.com>
2025-01-21 22:00:24 -0800

    fixed the missing notifications

--- MainModule/Client/UI/Library/Default/NotificationV2/init.lua
+++ MainModule/Client/UI/Library/Default/NotificationV2/init.lua
@@ -4,6 +4,8 @@ return function(envArgs, data)
 	local getEnv = envArgs.getEnv
 	local variables = envArgs.variables
 	local notifContainer = variables.notifV2Container
+	
+	local Signal = client.Signal
 
 	if not notifContainer then
 		notifContainer = client.UI.construct "Handlers.Notifications"
@@ -14,6 +16,15 @@ return function(envArgs, data)
 	notification.containerData = notifContainer
 
 	notifContainer:add(notification)
+	
+	if data.returnStateOnInteraction then
+		local didOpen = Signal:waitOnSingleEvents({notification.opened}, nil, 300)
+		if notification.showState then
+			task.defer(notification.hide, notification)
+		end
+		
+		return didOpen or false
+	end
 
 	return notification
 end
